002252a2d477b30db15291fb939f1ee0ab57e72e
dk9025396-jpg <dk9025396@gmail.com>
2026-05-28 01:12:14 +0530

    Create rg3.tf
    RG created 3 times

---
+++ rg3.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "rg" {
+    name = "rg1"
+    location = "central india"
+}resource "azurerm_resource_group" "rg1" {
+    name = "rg1"
+    location = "central india 1"
+}resource "azurerm_resource_group" "rg2" {
+    name = "rg2"
+    location = "central india 2"
+}
