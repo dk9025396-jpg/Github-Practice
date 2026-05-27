50045342551dfaa10aede3d765278f2c9c7ee518
dk9025396-jpg <dk9025396@gmail.com>
2026-05-28 01:15:16 +0530

    Update rg3.tf
    update

--- rg3.tf
+++ rg3.tf
@@ -1,10 +1,12 @@
 resource "azurerm_resource_group" "rg" {
     name = "rg1"
     location = "central india"
-}resource "azurerm_resource_group" "rg1" {
+}
+resource "azurerm_resource_group" "rg1" {
     name = "rg1"
     location = "central india 1"
-}resource "azurerm_resource_group" "rg2" {
+}
+resource "azurerm_resource_group" "rg2" {
     name = "rg2"
     location = "central india 2"
 }
